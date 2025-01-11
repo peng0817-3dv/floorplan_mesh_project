30ff9fba5967df7028f12e050c106e2876e03d96
PengDesktopWindows <wscs1042859069@163.com>
2025-01-11 13:07:52 +0800

    更新绘图，现在预测图和真实图在一张图上

--- test_code_playground/test_dataset.py
+++ test_code_playground/test_dataset.py
@@ -11,7 +11,7 @@ from dataset import sort_vertices_and_faces
 from dataset.floorplan_triangles import FPTriangleNodes,FPOriginTriangleNodes
 from util.s3d_data_load import read_s3d_mesh_info
 from util.s3d_data_process import process_vertice_by_op_record
-from util.visualization import export_mesh_to_shp, plot_vertices_and_faces_with_labels, export_mesh_to_obj
+from util.visualization import export_mesh_to_shp, plot_vertices_and_faces_with_labels, export_mesh_to_obj,plot_ground_truth_and_prediction
 import hydra
 
 from util.misc import scale_vertices, normalize_vertices, shift_vertices
@@ -27,6 +27,10 @@ def main(config):
     print(f"start:{dataset.get_name(0)}")
     print(f"end:{dataset.get_name(n-1)}")
     data1 = dataset[0]
+
+    _, targets, vertices, faces, _, op = dataset.get_all_features_for_shape(0)
+    plot_ground_truth_and_prediction(vertices=vertices, faces=faces, gt_labels=targets, pred_labels=targets,output_path=r"test.png")
+
     print("done")
 
 if __name__ == '__main__':
