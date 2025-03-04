9b8b94d6b7726bf4ac902cf5ef164677096f2ed6
PengDesktopWindows <wscs1042859069@163.com>
2025-03-04 21:56:02 +0800

    主训练模型添加角度损失

--- trainer/train_triangle.py
+++ trainer/train_triangle.py
@@ -7,6 +7,8 @@ from model.transformer_base import TransformerNet
 from cosine_annealing_warmup import CosineAnnealingWarmupRestarts
 from trainer import create_conv_batch, step, create_trainer
 from lightning_utilities.core.rank_zero import rank_zero_only
+import numpy as np
+from dataset.triangles import angle as angle_func
 
 
 class GraphTransformerEncoder(pl.LightningModule):
@@ -69,11 +71,20 @@ class GraphTransformerEncoder(pl.LightningModule):
 
         # 此处变为B X max_graph_size X num_cls
         decoded_x = decoded_x_conv.reshape(-1, decoded_x_conv.shape[-1])[conv_mask, :]  # num_triangles x num_cls(32)
-        loss = torch.nn.functional.cross_entropy(decoded_x.reshape(-1, decoded_x.shape[-1]), data.y.reshape(-1), reduction='mean')
+
+        # 计算损失
+        # 三角形分类损失（classify_loss）
+        classify_loss = torch.nn.functional.cross_entropy(decoded_x.reshape(-1, decoded_x.shape[-1]), data.y.reshape(-1), reduction='mean')
+        # 角度正则损失 (angle_loss)
+        angle_loss = self.angle_loss(data, decoded_x)
+        # 总损失
+        loss = self.config.angle_loss_weight * angle_loss + self.config.classify_loss_weight * classify_loss
 
         acc = self.get_accuracy(decoded_x, data.y)  # 计算准确度
-        self.log("train/cross_entropy_loss", loss.item(), on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True,batch_size=self.config.batch_size)
+        self.log("train/loss", loss.item(), on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True,batch_size=self.config.batch_size)
         self.log("train/acc", acc.item(), on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True,batch_size=self.config.batch_size)
+        self.log("train/classify_loss", classify_loss.item(), on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True,batch_size=self.config.batch_size)
+        self.log("train/angle_loss", angle_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True,batch_size=self.config.batch_size)
         loss = loss / self.config.gradient_accumulation_steps  # scale the loss to account for gradient accumulation
         self.manual_backward(loss)
         # accumulate gradients of `n` batches
@@ -81,6 +92,52 @@ class GraphTransformerEncoder(pl.LightningModule):
             step(optimizer, [self.encoder, self.FFN])
             optimizer.zero_grad(set_to_none=True)  # type: ignore
         self.log("lr", optimizer.param_groups[0]['lr'], on_step=True, on_epoch=False, prog_bar=False, logger=True, sync_dist=True)  # type: ignore
+
+
+    def angle_loss(self, data, decoded_x):
+        """
+        doc: 计算角度正则化损失
+        return: total_angle_loss: 角度正则化损失
+        """
+
+        # 1. 选出所有最大分类概率为room的三角形(及其索引)
+        softmax_decoded_x = torch.nn.functional.softmax(decoded_x, dim=1)  # 使用softmax得到不同分类的概率
+        max_probs, max_indices = torch.max(softmax_decoded_x, dim=1)
+        room_indices = torch.where(max_indices == 2)[0]
+
+        # 获取每个面的三个角 data.x_with_angle
+        vertices = data.x[:, :9].reshape(-1, 3, 3)  # 提取顶点坐标(128, 3, 3)
+        angles = angle_func(vertices)  # 计算内角 (128, 3)
+
+        # 2. 取它们的顶点(去重)
+        room_vertices = vertices[room_indices].reshape(-1, 3)  # (num_room_triangles * 3, 3)
+        room_angles = angles[room_indices].reshape(-1)  # (num_room_triangles * 3)
+        # 从 softmax_decoded_x 中提取出属于“room”类别的三角形的概率分布，并将每个三角形的概率分布重复 3 次，以对应每个三角形的 3 个顶点
+        room_decoded_x = softmax_decoded_x[room_indices].repeat_interleave(3, dim=0)  # (num_room_triangles * 3, num_cls)
+
+        # 3. 计算每个顶点的角度总和和融合概率特征
+        unique_vertices, inverse_indices = torch.unique(room_vertices, dim=0, return_inverse=True)
+        angle_merge = torch.zeros(unique_vertices.shape[0], dtype=torch.float32, device=room_vertices.device)
+        p_merge = torch.zeros(unique_vertices.shape[0], softmax_decoded_x.shape[1], dtype=torch.float32, device=room_vertices.device)
+
+        angle_merge.scatter_add_(0, inverse_indices, room_angles)
+        p_merge.scatter_add_(0, inverse_indices.unsqueeze(1).expand(-1, softmax_decoded_x.shape[1]), room_decoded_x)
+        p_merge = p_merge / torch.bincount(inverse_indices).unsqueeze(1).float()
+
+        # 4. angle_loss = p^alpha * min|θ - t|
+        t_values = torch.tensor([np.pi/2, np.pi, 3*np.pi/2, 2*np.pi], dtype=torch.float32, device=angle_merge.device)
+        alpha = self.config.angle_loss_alpha
+        total_angle_loss = 0
+
+        for angle_sum, p in zip(angle_merge, p_merge):
+            abs_diffs = torch.abs(angle_sum - t_values)
+            min_abs_diff = torch.min(abs_diffs)
+            angle_loss = p[2]**alpha * min_abs_diff
+            total_angle_loss += angle_loss
+
+        return total_angle_loss
+
+
 
     def validation_step(self, data, batch_idx):
         # encoder
@@ -96,15 +153,26 @@ class GraphTransformerEncoder(pl.LightningModule):
         decoded_x_conv = self.FFN(encoded_x_conv)
 
         decoded_x = decoded_x_conv.reshape(-1, decoded_x_conv.shape[-1])[conv_mask, :] # num_triangles x num_cls(32)
-        loss = torch.nn.functional.cross_entropy(decoded_x.reshape(-1, decoded_x.shape[-1]), data.y.reshape(-1), reduction='mean')
+
+        # 计算损失
+        # 三角形分类损失（classify_loss）
+        classify_loss = torch.nn.functional.cross_entropy(decoded_x.reshape(-1, decoded_x.shape[-1]),
+                                                          data.y.reshape(-1), reduction='mean')
+        # 角度正则损失 (angle_loss)
+        angle_loss = self.angle_loss(data, decoded_x)
+        # 总损失
+        loss = self.config.angle_loss_weight * angle_loss + self.config.classify_loss_weight * classify_loss
+
         # y_coords = data.y / (self.config.num_tokens - 3) - 0.5
         # loss_c = torch.nn.functional.mse_loss(decoded_c, y_coords, reduction='mean')
         acc = self.get_accuracy(decoded_x, data.y)
         # acc_triangle = self.get_triangle_accuracy(decoded_x, data.y)
         if not torch.isnan(loss).any():
-            self.log(f"val/cross_entropy_loss", loss.item(), add_dataloader_idx=False, on_step=False, on_epoch=True, prog_bar=False, logger=True, sync_dist=True,batch_size=self.config.batch_size)
+            self.log(f"val/loss", loss.item(), add_dataloader_idx=False, on_step=False, on_epoch=True, prog_bar=False, logger=True, sync_dist=True,batch_size=self.config.batch_size)
         if not torch.isnan(acc).any():
             self.log(f"val/acc", acc.item(), add_dataloader_idx=False, on_step=False, on_epoch=True, prog_bar=False, logger=True, sync_dist=True,batch_size=self.config.batch_size)
+        self.log("val/classify_loss", classify_loss.item(), on_step=False, on_epoch=True, prog_bar=False, logger=True, sync_dist=True,batch_size=self.config.batch_size)
+        self.log("val/angle_loss", angle_loss, on_step=False, on_epoch=True, prog_bar=False, logger=True, sync_dist=True,batch_size=self.config.batch_size)
 
     def inference_data(self, data):
         encoded_x = self.encoder(data.x.to(self.device), data.edge_index.to(self.device), torch.zeros([data.x.shape[0]],device=self.device).long())
